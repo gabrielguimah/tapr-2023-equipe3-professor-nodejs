1b47c560b54e636b98f02864abe61fccd3210156
Gabriel Guimarães da Rocha <78444679+gabrielguimah@users.noreply.github.com>
2023-11-13 00:47:46 +0000

    Rotas + controllers + api.rest + serviços

--- microserviceprofessor/server/api/controllers/professores/professorcontroller.ts
+++ microserviceprofessor/server/api/controllers/professores/professorcontroller.ts
@@ -1,8 +1,21 @@
 import { Request, Response } from 'express';
+import ProfessorService from '../../services/professor.service';
 
 export class ProfessorController {
     all(_: Request, res: Response): void {
-        res.json([]);
+        ProfessorService.all().then((r) => res.json(r));
+    }
+    getById(req: Request, res: Response): void {
+        ProfessorService.getById(req.params['id']).then((r) => res.json(r));
+    }
+    post(req: Request, res: Response): void {
+        ProfessorService.saveNew(req.body).then((r) => res.json(r));
+    }
+    update(req: Request, res: Response): void {
+        ProfessorService.update(req.params['id'], req.body).then((r) => res.json(r));
+    }
+    delete(req: Request, res: Response): void {
+        ProfessorService.delete(req.params['id']).then((r) => res.json(r));
     }
 }
 
---
+++ microserviceprofessor/server/api/services/professor.service.ts
@@ -0,0 +1,75 @@
+import { Container, SqlQuerySpec } from "@azure/cosmos";
+import cosmosDb from "../../common/cosmosdb";
+import { Professor } from "../entities/professor";
+
+export class ProfessorService {
+    private container: Container =
+        cosmosDb.container("professor");
+
+    async all(): Promise<Professor[]> {
+        const { resources: listaProfessores } =
+            await this.container.items.readAll<Professor>().fetchAll();
+
+        return Promise.resolve(listaProfessores);
+    }
+
+    async getById(id: string): Promise<Professor> {
+        const querySpec: SqlQuerySpec = {
+            query: "SELECT * FROM Professor c WHERE c.id = @id",
+            parameters: [
+                { name: "@id", value: id }
+            ]
+        };
+
+        const { resources: listaProfessores }
+            = await this.container.items.query(querySpec).fetchAll();
+
+        return Promise.resolve(listaProfessores[0]);
+    }
+
+    async saveNew(professor: Professor): Promise<Professor> {
+        professor.id = "";
+        await this.container.items.create(professor);
+
+        return Promise.resolve(professor);
+    }
+
+    async update(id: string, professor: Professor): Promise<Professor> {
+        const querySpec: SqlQuerySpec = {
+            query: "SELECT * FROM Professor c WHERE c.id = @id",
+            parameters: [
+                { name: "@id", value: id }
+            ]
+        };
+
+        const { resources: listaProfessores }
+            = await this.container.items.query(querySpec).fetchAll();
+
+        const professorAntigo = listaProfessores[0];
+
+        professorAntigo.nome = professor.nome;
+        await this.container.items.upsert(professorAntigo)
+
+        return Promise.resolve(professorAntigo);
+    }
+
+    async delete(id: string): Promise<string> {
+        const querySpec: SqlQuerySpec = {
+            query: "SELECT * FROM Professor c WHERE c.id = @id",
+            parameters: [
+                { name: "@id", value: id }
+            ]
+        };
+
+        const { resources: listaProfessores }
+            = await this.container.items.query(querySpec).fetchAll();
+            
+        for (const professor of listaProfessores) {
+            await this.container.item(professor.id, professor.id).delete();
+        }
+
+        return Promise.resolve(id);
+    }
+}
+
+export default new ProfessorService();
